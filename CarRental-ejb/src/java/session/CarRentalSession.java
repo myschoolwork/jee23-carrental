2c946cc6585fd00db5deb4bfb5cc565787f28ad8
Pablo Bollansée <pablo.bollansee@gmail.com>
2015-11-10 14:55:03 +0100

    Used transactions in confirm quotes.

--- CarRental-ejb/src/java/session/CarRentalSession.java
+++ CarRental-ejb/src/java/session/CarRentalSession.java
@@ -5,11 +5,11 @@ import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
+import javax.ejb.EJBException;
 import javax.ejb.Stateful;
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
+import javax.ejb.TransactionAttribute;
+import static javax.ejb.TransactionAttributeType.REQUIRES_NEW;
 import javax.persistence.TemporalType;
-import rental.CarRentalCompany;
 import rental.CarType;
 import rental.Quote;
 import rental.Reservation;
@@ -76,6 +76,7 @@ public class CarRentalSession extends Session implements CarRentalSessionRemote 
     }
 
     @Override
+    @TransactionAttribute
     public List<Reservation> confirmQuotes() throws ReservationException {
         List<Reservation> done = new LinkedList<Reservation>();
         try {
@@ -83,9 +84,9 @@ public class CarRentalSession extends Session implements CarRentalSessionRemote 
                 done.add(getCompany(quote.getRentalCompany()).confirmQuote(quote));
             }
         } catch (Exception e) {
-            for(Reservation r:done)
-                getCompany(r.getRentalCompany()).cancelReservation(r);
-            throw new ReservationException(e);
+            // throw EJBException so the container known to rollback
+            // alternative: use setRollbackOnly and throw other exception
+            throw new EJBException(e);
         }
         return done;
     }
